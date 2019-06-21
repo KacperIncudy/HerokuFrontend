35134f5a6512e746574db341e6f3b604d5464ee4
Colin Finger <finger.colin@gmail.com>
2019-06-21 10:56:46 +0200

    validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -12,7 +12,8 @@ class Register extends React.Component {
     this.state={
       email: '',
       password: '',
-      name: ''
+      name: '',
+      notAnEmail: false
     }
   }
 
@@ -41,15 +42,20 @@ onSubmitSignIn = () => {
     })
     .then(response => response.json())
     .then(user => {
-      if(user.id) {
+      if(user.email.contains('@') && user.email.contains('.')) {
           this.props.loadUser(user);
          this.props.onRouteChange('home');
-       } 
+       } else {
+        this.setState = ({
+          notAnEmail: true
+        });
+       }
     })
   }
 
 
   render() {
+    if(this.state.notAnEmail === false) {
 	return(
 		<article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-1 mw6 shadow-5 center'>
 		 <main className="pa4 black-80">
@@ -98,7 +104,57 @@ onSubmitSignIn = () => {
   </div>
 </main>
 </article>
-		);
+		); } else {
+    return(
+    <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-1 mw6 shadow-5 center'>
+     <main className="pa4 black-80">
+  <div className="measure">
+    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+      <legend className="f1 fw6 ph0 mh0">Register</legend>
+      <div className="mt3">
+        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+        <input 
+        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+        type="text" 
+        name="name"  
+        id="name" 
+        onChange={this.onNameChange}
+        />
+      </div>
+      <div className="mt3">
+        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+        <input 
+        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+        type="email" 
+        name="email-address"  
+        id="email-address" 
+        onChange={this.onEmailChange}
+        />
+      </div>
+      <div className="mv3">
+        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+        <input 
+        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+        type="password" 
+        name="password"  
+        id="password" 
+        onChange={this.onPasswordChange}
+        />
+      </div>div className="mv3"><p>Email must contain "@"" and "."</p></div>
+    </fieldset>
+    <div className="">
+      <input 
+      onClick={this.onSubmitSignIn}
+      className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+      type="submit" 
+      value="Register" 
+      />
+    </div>
+  </div>
+</main>
+</article>
+    );
+  }
 }
 }
 
